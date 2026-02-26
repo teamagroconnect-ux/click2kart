c60f94106aec59e7c5aa8bb7334cc40e5515dc29
uddhabdas <uddhabdas2020@gmail.com>
2026-02-26 09:15:05 +0530

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
+import colors from 'tailwindcss/colors'
+
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        blue: colors.violet,
+        emerald: colors.violet,
+        purple: colors.violet
+      }
+    }
+  },
   plugins: [],
 };
